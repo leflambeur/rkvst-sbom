10a6dfb623f0125783ef8941d5c5068b8b8d0b4f
William Godfrey <sernineleflambeur@gmail.com>
2021-12-20 17:45:41 +0000

    test-rearch

--- sbom.py
+++ sbom.py
@@ -1,126 +1,98 @@
-from software_package import SoftwarePackage
-from archivist import archivist
-import sys
 import json
 import os
-import requests
+import argparse
 
-def generate_token():
+from software_package import SoftwarePackage
+from archivist import archivist
 
+def loadJSON(jsonInput):
     try:
-        client_id = os.getenv("CLIENT_ID")
-        client_secret = os.getenv("CLIENT_SECRET")
-    except:
-        exit( "Error: Missing CLIENT_ID or CLIENT_SECRET.")
-
-    print("Found Credentials")
-
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-    params = {
-        "grant_type": "client_credentials",
-        "client_id": client_id,
-        "client_secret": client_secret,
-        
-    }
-
-    print("Generating Token")
-    token_request = requests.post("https://app.rkvst.io/archivist/iam/v1/appidp/token", headers=headers, data=params).json()
-
-    return token_request.get("access_token")
-    
-
+        validJSON = json.loads(jsonInput)
+    except ValueError:
+        exit(
+            'Invalid JSON'
+        )
+    return validJSON
 
 def main():
 
-    command = sys.argv[1]
-    asset_id = sys.argv[2]
-    try:
-            authtoken = generate_token()
-    except:
-        exit(
-            "ERROR: Auth token not found. Please check your CLIENT_ID and AUTH_TOKEN."
-        )
+    parser = argparse.ArgumentParser(description='RKVST SBOM CLI Tool')
 
-    print("Token Generated")
+    parser.add_argument('--url', '-u', default='https://app.rkvst.io', help='URL to upload to')
+    parser.add_argument('--assetID', '-id', nargs=1, help='The Asset ID being submitted to')
+    parser.add_argument('--command', '-cmd', nargs=1, default='release', help='The type of event being submitted')
+    parser.add_argument('--requiredAttributes', '-req', help='Required Event Attributes JSON')
+    parser.add_argument('--customEvent', '-evt', help='Custom Event Attributes JSON')
+    parser.add_argument('--latestSBOM', '-sbom', help='Latest SBOM Attributes JSON')
+    parser.add_argument('--customAsset', '-asset', help='Custom Asset Attributes JSON')
+    parser.add_argument('attachments', metavar='<sbom-file>', nargs='+', help='SBOM to be uploaded')
+
+    client_group = parser.add_mutually_exclusive_group(required=True)
+    client_group.add_argument('--clientId', '-c', nargs=1, help='Specify Application CLIENT_ID inline')
+    client_group.add_argument('--envClientId', '-i', action='store_true',  help='Specify if your Application CLIENT_ID is an Env Var')
+
+    secret_group = parser.add_mutually_exclusive_group(required=True)
+    secret_group.add_argument('--secret', '-s', nargs=1, help='Specify Application SECRET inline')
+    secret_group.add_argument('--envSecret', '-e', action='store_true',  help='Specify if your Application SECRET is an Env Var')
+
+    args = parser.parse_args()
+
+    command_options = ['release', 'release_plan', 
+        'release_accepted', 'patch', 'private_patch', 
+        'vuln_disclosure', 'vuln_update', 'vuln_report', 
+        'deprecation']
+
+    command = args.command
     
-    arch = archivist.Archivist(
-        "https://app.rkvst.io",
-        auth=authtoken,
-        )
-    
+    if args.envClientId == True:
+        client_id = os.getenv("CLIENT_ID")
+        if client_id is None:
+            exit(
+                "ERROR: CLIENT_ID EnvVar not found"
+            )
+    else:
+        client_id = args.clientId
+
+    if args.envSecret == True:
+        client_secret= os.getenv("SECRET")
+        if client_secret is None:
+            exit(
+                "ERROR: SECRET EnvVar not found"
+            )
+    else:
+        client_secret = args.secret
+
+    rkvst_url = args.url
+
+    arch = archivist(
+        rkvst_url,
+        (client_id, client_secret),
+    )
+
+    asset_id = args.assetID
+
     package = SoftwarePackage(arch)
 
     package.read(asset_id)
 
     print("Doing " + command)
-     
-    match command:
 
-        case 'release':
-            sbom=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            latest_sbom=json.loads(sys.argv[6])
-            custom_asset_attrs=json.loads(sys.argv[7])
-            release = package.release(sbom=sbom, attachments=attachments, custom_attrs=custom_attrs, latest_sbom=latest_sbom, custom_asset_attrs=custom_asset_attrs)
+    for command in command_options:
+        attrs=loadJSON(args.requiredAttributes)
+        attachments=loadJSON(args.attachments)
+        custom_attrs=loadJSON(args.customEvent)
+        if command == 'release':
+            latest_sbom=loadJSON(args.latestSBOM)
+            custom_asset_attrs=loadJSON(args.customAsset)
+            release = package.command(attrs=attrs, attachments=attachments, custom_attrs=custom_attrs, latest_sbom=latest_sbom, custom_asset_attrs=custom_asset_attrs)
             print("Release Complete\n\n" + str(release))
-
-        case 'release_plan':
-            sbom_planned=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            release_plan = package.release_plan(sbom_planned=sbom_planned, attachments=attachments, custom_attrs=custom_attrs)
-            print("Release Plan Complete\n\n" + str(release_plan))
-        
-        case 'release_accepted':
-            sbom_accepted=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            release_accepted = package.release_accepted(sbom_accepted=sbom_accepted, attachments=attachments, custom_attrs=custom_attrs)
-            print("Release Accepted Complete\n\n" + str(release_accepted))
-
-        case 'patch':
-            sbom_patch=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            patch = package.patch(sbom_patch=sbom_patch, attachments=attachments, custom_attrs=custom_attrs)
-            print("Patch Complete\n\n" + str(patch))
-
-        case 'private_patch':
-            sbom_patch=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            private_patch = package.private_patch(sbom_patch=sbom_patch, attachments=attachments, custom_attrs=custom_attrs)
-            print("Private Patch Complete\n\n" + str(private_patch))
-        
-        case 'vuln_disclosure':
-            vuln=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            vuln_disclosure = package.vuln_disclosure(vuln=vuln, attachments=attachments, custom_attrs=custom_attrs)
-            print("Vulnerability Disclosure Complete\n\n" + str(vuln_disclosure))
-
-        case 'vuln_update':
-            vuln=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            vuln_update = package.vuln_update(vuln=vuln, attachments=attachments, custom_attrs=custom_attrs)
-            print("Vulnerability Update Complete\n\n" + str(vuln_update))
-
-        case 'vuln_report':
-            vuln=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            vuln_report = package.vuln_report(vuln=vuln, attachments=attachments, custom_attrs=custom_attrs)
-            print("Vulnerability Report Complete\n\n" + str(vuln_report))
-
-        case 'deprecation':
-            sbom_eol=json.loads(sys.argv[3])
-            attachments=json.loads(sys.argv[4])
-            custom_attrs=json.loads(sys.argv[5])
-            deprecation = package.deprecation(sbom_eol=sbom_eol, attachments=attachments, custom_attrs=custom_attrs)
-            print("Deprecation Complete\n\n" + str(deprecation))
+        else:
+            event = package.command(attrs=attrs, attachments=attachments, custom_attrs=custom_attrs)
+            print("Event Complete\n\n" + str(event))
+    else:
+        exit(
+            'Invalid Command'
+            )
 
 if __name__ == "__main__":
     main()
